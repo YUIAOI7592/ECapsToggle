52f227cbeb7a2ba37fce010751af98db05ff1f1c
YUIAOI <qwe153246@gmail.com>
2017-10-19 21:10:07 -0500

    Update index.js

--- index.js
+++ index.js
@@ -13,5 +13,4 @@ function file() {
 	if (process.argv[2] === undefined) throw new Error('You need to open a file.');
 	var fileLink = path.normalize(process.argv[2]);
 	var writeFile = path.dirname(fileLink) + path.sep + path.basename(fileLink, path.extname(fileLink)) + '_Inverse' + path.extname(fileLink);
-	
 }
